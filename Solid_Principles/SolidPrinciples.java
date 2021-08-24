7c75ffea5e37430a976ae6bac7c5655ae865fbd7
Ambati Shiva <shiva.ambati@zemosolabs.com>
2021-08-24 15:19:04 +0530

    Solid Principles Assignmet Modified

--- Solid_Principles/SolidPrinciples.java
+++ Solid_Principles/SolidPrinciples.java
@@ -1,202 +1,210 @@
-/*--------------Each Class in the following Program Has Single Responsibility--------*/
-class Bookings            
-{
-	payment p;
-	public void Bookings(payment p)
-	{
-    //payment p=new payment();
-    this.p=p;
-	p.save_details();
-	}	
-  public boolean availabilty()
-  {
-	  System.out.println("Enter Destination");
-      return true;
-  }
-  public boolean make_payment()
-  {
-	  System.out.println("Choose payment mode");
-      return true;
-  }
- }
- 
- 
- 
-/*-------------- Open For Extension Closed for Modification --------------*/
+/* --------------------------SIMPLE LIBRARY MANAGEMENT PROGRAM------------------------------------------*/
 
-
-
-interface Make_payments             
-{
-public void make_payment();
-}
-
-
-
- class Netbanking implements Make_payments
-{   public String make_payment(int n)
-	{
-	return "Successfully paid Rs."+n;
-	}
-}
- class Card implements Make_payments
-{   public String make_payment()
-	{
-	return "Successfully paid Rs."+n;
-	}
-}
+/* All The Classes in this Program Follows Single Responsilbilty Principle*/
 
 
 
 
-/*---------------Liskov's Principle--------------*/
 
- class Vehicle
-{
-    
-    int avail;
-    public int seats_available(int booked, int total)
-    {
-         this.avail=total-booked;
-         return this.avail;
-    }
-    public int total_price(int dist,int cost)
-    {
-        return dist*cost;
-    }
-}
-class Bus extends Vehicle
-{
-     public int seats_available(int booked, int total)
-    {
-         this.avail=total-booked;
-         return this.avail;
-    }
-    public int total_price(int dist,int cost)
-    {
-        return dist*cost;
-    }
-}
+//------------------OPEN FOR EXTENSION CLOSED FOR MODIFICATION PRINCIPLE--------------------------
 
- class Car extends Vehicle
-{    int avail=0;
-     public int seats_available(int booked, int total)
-    {
-         this.avail=total-booked;
-         return this.avail;
-    }
-    public int total_price(int dist,int cost)
-    {
-        return dist*cost;
-    }
-}
-
-/*-----------------------Interface Segregation Principle------------------*/
+interface Login{
 	
-	interface Holiday_trip
-{
-	public void food_menu();
-	public void accomodatioin_details();
+	public boolean login();
 }
 
- interface Causual_trip
+class Student_LogIn
 {
-	public void snacks_and_waterbottle();
-	public void newspaper();
-}
-
- class Holiday_Trip implements Causual_trip, Holiday_trip
-{
-	public void food_menu()
+	public boolean login(String id, String password)
 	{
-		System.out.println("Select Food itemsfrom [Biryani, Burger, Pizza]");
-	}
-	public void accomodatioin_details()
-	{
-		System.out.println("Mention Hotel specifiactions");
-	}
-	public void newspaper()
-	{
-        System.out.println("Select yes/no: ");
-	}
-    public void snacks_and_waterbottle()
-	{
-		System.out.println("Select Yes/No: ");
-	}
-}
- class Casual_Trip implements Causual_trip
-{
-	public void newspaper()
-	{
-        System.out.println("Select yes/no");
-	}
-    public void snacks_and_waterbottle()
-	{
-		System.out.println("Select Yes/No");
+		System.out.println("Enter Student Id: "+id);
+		System.out.println("Enter password: "+password);
+		return true;
 		
 	}
 }
- class payment{
-	boolean t;
-    public void save_details()
-    {
-    System.out.println("Saved Succesfully");
-    }
-}
 
-/*-------------Dependency Inversion Principle----------------------*/
- 
- class Bookings            
+
+class Faculty_LogIn
 {
-	payment p;
-	public void Bookings(payment p)
+	public boolean login(String id, String password)
 	{
-    //payment p=new payment();
-    this.p=p;
-	p.save_details();
-	}	
-  public boolean availabilty()
-  {
-	  System.out.println("Enter Destination");
-      return true;
-  }
-  public boolean make_payment()
-  {
-	  System.out.println("Choose payment mode");
-      return true;
-  }
- }
-
-	
-
-
-public class Main {
-  public static void main(String[] args) {
-    //System.out.println("Hello World!");
-    payment p=new payment();
-    Bookings b=new Bookings(p);
-	
-	
-	
-    Netbanking n=new Netbanking();
-    System.out.println("State of Payment :"+n.make_payment(100));
-    Card c=new Card();
-    System.out.println("State of Payment :"+c.make_payment(200));
-	
-	
-	 
-    Vehicle v1= new Bus();
-    System.out.println("The Available Seats in the Bus are "+v1.seats_available(20,40));
-    Vehicle v2= new Car();
-    System.out.println("The Cost of Hhiring car for 10KM is "+v2.total_price(10,10));
-	
-	
-	Holiday_Trip h=new Holiday_Trip();
-System.out.println(h.newspaper());
-System.out.println(h.snacks_and_waterbottle());
-Causual_trip ct=new Causual_trip();
-System.out.println(ct.newspaper());
-    
-  }
+		System.out.println("Enter Faculty Id: "+id);
+		System.out.println("Enter password: "+password);
+		return true;
+		
+	}
 }
 
+class Guest_LogIn
+{
+	public boolean login(String id, String otp)
+	{
+		System.out.println("Enter E-mail Id: "+id);
+		System.out.println("Enter OTP: "+otp);
+		return true;
+		
+	}
+}
+
+//----------------------LISKOV'S SUBSTITUITON PRINCIPLE-----------------------------------
+
+class Details
+{
+	int[] status= new int[]{ 1,0,1,0,1,1,1,1,1,1 };
+	int[] pages= new int[]{ 100,200,123,201,123,156,178,190,111,120 };
+	int[] vol= new int[]{ 1,2,3,4,5,4,4,3,2,1 };
+	
+	void details(int id)
+	{
+	 System.out.println("The Item with "+this.status[id]  +" is available");
+     System.out.println("Number of Pages :" +this.pages[id]);
+	  System.out.println("No. of Volumues available:" +this.vol[id]);
+	}
+}
+	 
+	
+class Book_Details extends Details
+{
+	
+    int[] status= new int[]{ 1,0,1,0,1,1,1,1,1,1 };
+	int[] pages= new int[]{ 100,200,123,201,123,156,178,190,111,120 };
+	int[] vol= new int[]{ 1,2,3,4,5,4,4,3,2,1 };
+	
+	void details(int book_id)
+	{
+	 System.out.println("The Book with "+this.status[book_id]  +"available");
+     System.out.println("Number of Pages :" +this.pages[book_id]);
+	  System.out.println("No. of Volumues available:" +this.vol[book_id]);
+	}
+}
+
+class Journal_Details extends Details
+{
+	
+    int[] status= new int[]{ 1,0,1,0,1,1,1,1,1,1 };
+	int[] pages= new int[]{ 100,200,123,201,123,156,178,190,111,120 };
+	int[] vol= new int[]{ 1,2,3,4,5,4,4,3,2,1 };
+	
+	void details(int jrnl_id)
+	{
+	 System.out.println("The Book with "+this.status[jrnl_id]  +"available");
+     System.out.println("Number of Pages :" +this.pages[jrnl_id]);
+	  System.out.println("No. of Volumues available:" +this.vol[jrnl_id]);
+	}
+
+}
+
+//-------------------------------INTERFACE SEGREGATION PRINCIPLE-----------------------------------
+
+interface student_services
+{
+	public void book_issue(int id);
+	public void book_return(int id);
+	
+}
+interface faculty_services
+{
+   public void publish_journal(int id);
+   public void publish_book(int id);
+}
+
+class Student_Services implements student_services
+{
+	public void book_issue(int id)
+	{
+		System.out.println("The Book with "+id +" is Issued Succesfully");
+	}
+	
+    public void book_return(int id)
+	{
+		System.out.println("The Book with "+id +" is Returned Succesfully");
+	}
+	
+}
+
+class Faculty_Services implements student_services, faculty_services
+{
+	public void book_issue(int id)
+	{
+		System.out.println("The Book with "+id +" is Issued Succesfully");
+	}
+	
+    public void book_return(int id)
+	{
+		System.out.println("The Book with "+id +" is Returned Succesfully");
+	}
+	 
+	public void publish_journal(int id)
+	{
+		System.out.println("The Jounal with "+id +" has published Succesfully");
+	}
+	 public void publish_book(int id)
+	{
+		System.out.println("The Book with "+id +" has Published Succesfully");
+	}
+}
+
+	
+//-------------------------------DEPENDENCY INVERSION PRINCIPLE------------------------------------
+
+class Librarian
+{
+	void Add_new_resource(Resource r)
+	{
+		r.check_status();
+	}
+}
+
+class Resource
+{
+	
+    void check_status()	
+   {
+	System.out.println("New resource added to the Library Successfully");
+   }
+}
+
+class Add_new_Book
+{
+	void add_new_book()
+	{
+	Resource r=new Resource();
+	Librarian l= new Librarian();
+	l.Add_new_resource(new Resource());
+	
+	}
+}
+
+
+//-------------------------------MAIN METHOD-------------------------------------------------------
+
+public class soliddemo
+{
+	public static void main(String aa[])
+	{
+		
+		
+	 Student_LogIn s=new  Student_LogIn();	
+	 s.login("Shiva","s@123");
+	 
+	 
+	 Details D1=new Book_Details();
+	 D1.details(5);
+	 Details D2=new Journal_Details();
+	 D2.details(4);
+	 
+	 
+	 Student_Services ss=new Student_Services();
+	 ss.book_issue(542);
+	 Faculty_Services fs=new Faculty_Services();
+	 fs.publish_journal(525);
+	 
+	 
+	 
+	 Add_new_Book a=new Add_new_Book();
+	 a.add_new_book();
+	}
+}
+	 
+	 
